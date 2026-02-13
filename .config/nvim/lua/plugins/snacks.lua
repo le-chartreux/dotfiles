a9426c237f71f69d3c32b75fc6d1eda4914e7eb3
le-chartreux <le-chartreux-vert@protonmail.com>
2026-02-13 18:30:20 +0100

    nvim: show hidden files in explorer but hide those in .gitignore

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        git = {
+          ignore = true, -- hide files in .gitignore
+        },
+      },
+      picker = {
+        hidden = true, -- show files starting by .
+      },
+    },
+  },
+}
+
