22665d088af8043982a65c9360bd5f5827a45c5c
le-chartreux <le-chartreux-vert@protonmail.com>
2026-04-14 20:33:43 +0200

    nvim: stop hidding links in markdown

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -8,3 +8,11 @@
 
 -- Remove spellchecks.
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Stop hiding links in markdown.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
